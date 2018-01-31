8b0ca6049b2a8e034dcc1e72cac1cd047b6d53c9
Stephen Board <sboard@lion.lmu.edu>
2018-01-30 21:10:23 -0800

    Trouble recalling cpp. So baby step! It builds.
    Made  struct to define points. Made a vector of these points.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,50 @@
 //  Copyright © 2018 Stephen Board. All rights reserved.
 //  not really
 
+#include <vector>
 #include <iostream>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#include "Chaikins.hpp"
+
+
+using namespace std;
+
+struct Coordinate{
+    GLfloat x, y;
+    Coordinate(GLfloat x_, GLfloat y_) : x(x_), y(y_) {}
+};
+
+Coordinate a = Coordinate(-0.4f, 0.0f);
+Coordinate b = Coordinate(0.0f, 0.6f);
+Coordinate c = Coordinate(0.4f, 0.0f);
+
+vector<Coordinate> points = {a, b, c};
+
+
+
+
+
+                          
+
+
+
+
+
+/*
+    //Coordinate(GLfloat, GLfloat);
+    GLfloat get_y() {return y; };
+    GLfloat get_x() {return x; };
+};
+   
+
+Vertex::Vertex(GLfloat X, GLfloat Y){
+    x = X;
+    y = Y;
+};
+
+*/
 
 void setup() {
     
@@ -19,51 +59,25 @@ void setup() {
 
 
 
+
 void display(){
     //where we do all the drawings
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glColor3f(0.0f, 0.0f, 0.0f); //set our color to BLACK
     
-    // draw 4 points wtih coordinates (x,y) counter clockwise
-    // (0.5,0.5), (-0.5,0.5), (0.5,-0.5), (-0.5,-0.5)
-    // origin is at 0,0
-    glBegin(GL_POINTS);
-//    glVertex2f(0.5f, 0.5f);
-//    glVertex2f(-0.5f, 0.5f);
-//    glVertex2f(-0.5f, -0.5f);
-//    glVertex2f(0.5f, -0.5f);
-    glEnd();
+
     
-    //draw the holy cross
+    vector<Vertex> v;
     
     glBegin(GL_LINES);
     
-    glVertex2f(0.0f, 0.0f);
+    glVertex2f(-0.4f, 0.0f);
     glVertex2f(0.0f, 0.6f);
     
-    glVertex2f(-0.45f, 0.6f);
     glVertex2f(0.0f, 0.6f);
-    
-    glVertex2f(0.0f, 0.0f);
-    glVertex2f(0.0f, -0.6f);
-    
-    glVertex2f(0.45f, -0.6f);
-    glVertex2f(0.0f, -0.6f);
-    
-    glVertex2f(0.0f, 0.0f);
-    glVertex2f(-0.45f, 0.0f);
-    
-    glVertex2f(-0.45f, 0.0f);
-    glVertex2f(-0.45f, -0.6f);
-    
-    glVertex2f(0.0f, 0.0f);
-    glVertex2f(0.45f, 0.0f);
-    
-    glVertex2f(0.45f, 0.0f);
-    glVertex2f(0.45f, 0.6f);
-    glLineWidth(20.5f);
-    
+    glVertex2f(0.4f, 0.0f);
+
     glEnd();
     glutSwapBuffers();
 }
@@ -77,8 +91,11 @@ int main(int argc, char * argv[]) {
     glutInitWindowSize(800,600);
     glutCreateWindow("My Window");
     setup();
-    glutDisplayFunc(display);
-    glutMainLoop();
+    //glutDisplayFunc(display);
+    //glutMainLoop();
+    GLfloat x=0.6, y=0.4;
+    Coordinate P(x,y);
+    std::cout << P.x<<"\n";
     
     
     
